587612bde3228088323062f301f71b20890fa16f
Daniel Prevost <dprevost@users.sourceforge.net>
2009-08-16 21:03:59 -0400

    Fix most of the problems with errorparser integration with waf.

--- Configure/errorparser.py
+++ Configure/errorparser.py
@@ -7,6 +7,55 @@ import os, os.path
 import pproc
 import Task, Utils, Node, Constants
 from TaskGen import feature
+import xml.dom.minidom
+
+def getValue(nodelist):
+   '''
+   Retrieve the value associated with a specific xml tag
+   '''
+   value = ""
+   for node in nodelist:
+      if node.nodeType == node.TEXT_NODE:
+         value = node.data
+         return value.strip()
+   return value
+
+def getOptions(options):
+   '''
+   Extract all the generated output files (by error Parser) as they are 
+   defined in the xml option file.
+   
+   Note: this function is incomplete. Java, python and c# are not handled yet.
+   '''
+   outputFiles = []
+   dom = xml.dom.minidom.parse(options)
+
+   nodes = dom.getElementsByTagName('header_dirname')
+   if nodes.length > 0:
+      dir_name = getValue(nodes.item(0).childNodes)
+
+      nodes = dom.getElementsByTagName('header_name')
+      if nodes.length > 0:
+         outputFiles.append(dir_name + os.sep + getValue(nodes.item(0).childNodes))
+
+   nodes = dom.getElementsByTagName('errmsg_dirname')
+   if nodes.length > 0:
+      dir_name = getValue(nodes.item(0).childNodes)
+
+      nodes = dom.getElementsByTagName('errmsg_header_name')
+      if nodes.length > 0:
+         outputFiles.append(dir_name + os.sep + getValue(nodes.item(0).childNodes))
+   else:
+      nodes = dom.getElementsByTagName('errmsg_header_name')
+      if nodes.length > 0:
+         outputFiles.append(getValue(nodes.item(0).childNodes))
+
+   nodes = dom.getElementsByTagName('errmsg_c_fullname')
+   if nodes.length > 0:
+      outputFiles.append(getValue(nodes.item(0).childNodes))
+
+   dom.unlink()
+   return outputFiles
 
 class parser_task(Task.Task):
 
@@ -60,11 +109,16 @@ def process_parser(self):
    tsk = self.create_task('parser')
    tsk.set_inputs( [opt_node, xml_node] )
 
-   out1 = self.path.parent.find_resource('Nucleus/psoErrorHandler.c')
-   if not out1: raise ValueError, 'out1 file not found'
-   out2 = self.path.parent.find_resource('Nucleus/psoErrorHandler.h')
-   if not out2: raise ValueError, 'out2 file not found'
-   tsk.set_outputs( [out1, out2] )
+   outfiles = getOptions(opt_node.nice_path())
+   
+   outputs = []
+   for f in outfiles:
+      out = self.path.find_resource(Utils.split_path(f))
+      if not out: raise ValueError, f + ' file not found'
+      outputs.append(out)
+      
+   if len(outputs) > 0:
+      tsk.set_outputs( outputs )
 
 def detect(conf):
    conf.find_program('errorParser', var='ERROR_PARSER', mandatory=True)
